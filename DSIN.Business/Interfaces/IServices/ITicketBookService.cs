a765a64582367e380b2300679fc98ea813a8be90
Vinicius Gomes <viniciusfilhogomes@gmail.com>
2025-11-25 16:15:55 -0300

    Update ITicketBookService.cs

--- DSIN.Business/Interfaces/IServices/ITicketBookService.cs
+++ DSIN.Business/Interfaces/IServices/ITicketBookService.cs
@@ -4,6 +4,7 @@ namespace DSIN.Business.Interfaces.IServices;
 
 public interface ITicketBookService
 {
+    Task<TicketBookDetailsDto?> GetByIdAsync(Guid id, CancellationToken ct);
     Task<TicketBookResponseDto> AnalyzeAsync(Guid agentId, string imageBase64, CancellationToken ct);
     Task<PagedResult<TicketBookSummaryDto>> ListByAgentAsync(Guid agentId, int skip, int take, CancellationToken ct);
 }
